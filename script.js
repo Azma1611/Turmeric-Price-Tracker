eb2d151a5c2b8b262810e4c401f2fca6a23767e9
Azma <asmaalavudeen2646@gmail.com>
2026-01-20 19:46:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // script will be added step by step
+
+function saveAndNext() {
+  const text = document.getElementById("dataInput").value.trim();
+
+  if (text === "") {
+    alert("Please paste data first");
+    return;
+  }
+
+  let history = JSON.parse(localStorage.getItem("turmericHistory")) || [];
+
+  const today = new Date().toLocaleDateString();
+
+  history.push({
+    date: today,
+    raw: text
+  });
+
+  localStorage.setItem("turmericHistory", JSON.stringify(history));
+
+  window.location.href = "history.html";
+}
