87c23197a0230c04e6ce8aa3e3eee2ccde271f23
Azma <asmaalavudeen2646@gmail.com>
2026-01-20 19:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ function saveAndNext() {
 
   window.location.href = "history.html";
 }
+
+function loadHistory() {
+  const container = document.getElementById("historyContainer");
+  if (!container) return;
+
+  const history = JSON.parse(localStorage.getItem("turmericHistory")) || [];
+
+  if (history.length === 0) {
+    container.innerHTML = "<p>No data found</p>";
+    return;
+  }
+
+  history.reverse().forEach((item, index) => {
+    const div = document.createElement("div");
+    div.className = "historyBox";
+    div.innerHTML = `
+      <strong>${item.date}</strong>
+      <pre>${item.raw}</pre>
+    `;
+    container.appendChild(div);
+  });
+}
+
+loadHistory();
